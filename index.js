201658ebd540bc8b64bd5f0b739ceee28ce2e440
Addis Belete <addis.bel@gmail.com>
2021-12-30 16:25:59 +0300

    Try v-if and v-else directives

--- index.js
+++ index.js
@@ -2,20 +2,9 @@
 var vm = new Vue({
 	el: "#app",
 	data: {
-		firstName: "Foo",
-		lastName: "Bar",
-		fullName: "Foo Bar",
-	},
-	watch: {
-		firstName: function (val) {
-			this.fullName = val + ' ' + this.lastName
-		},
-		lastName: function (val) {
-			this.fullName = this.firstName + ' ' + val
-		}
+		awesome: true,
 
 	}
 
 })
-vm.firstName = 'Addis'
-vm.lastName = "Belete"
+
